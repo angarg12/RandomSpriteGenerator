79e0870e204cae803c8e8d1e9e57073cf927ec64
Andrés <angarg12@gmail.com>
2013-10-17 16:42:46 +0200

    Major ColorSchemeGenerator refactor
    Major ColorSchemeGenerator refactor plus bugs solved. Now the color
    schemes should be consistent

--- src/examples/tools/spritegen/ColorSchemeGenerator.java
+++ src/examples/tools/spritegen/ColorSchemeGenerator.java
@@ -3,16 +3,17 @@ package examples.tools.spritegen;
 import java.util.Random;
 
 public class ColorSchemeGenerator {
-	// https://lotsacode.wordpress.com/2010/03/11/hsvtorgb-and-rgbtohsv-in-c/
 	static Random random = new Random();
 	public static void fixRandom(int seed){
 		random = new Random(seed);
 	}
+	
+	// https://lotsacode.wordpress.com/2010/03/11/hsvtorgb-and-rgbtohsv-in-c/
 	/**
 	 * h,s,v between 0 and 1. XXX this algorithm has h=0-blue,
 	 * h=0.3333333-green, h=0.5-yellow
 	 **/
-	public static void HSVtoRGB(double h, double s, double v, int[] arr, int ofs) {
+	private static int HSVtoRGB(double h, double s, double v) {
 		double r = 0, g = 0, b = 0;
 		if (s == 0) {
 			r = v;
@@ -56,7 +57,7 @@ public class ColorSchemeGenerator {
 				b = var2;
 			}
 		}
-		arr[ofs] = (int) (r * 255.49) + ((int) (g * 255.49) << 8)
+		return (int) (r * 255.49) + ((int) (g * 255.49) << 8)
 				+ ((int) (b * 255.49) << 16);
 	}
 
@@ -74,76 +75,158 @@ public class ColorSchemeGenerator {
 			int colors, 
 			int shades) {
 		int[] colorScheme = new int[(colors + 3) * shades];
+		initializeColorScheme(colorScheme, transparent, black, shades, colors);
+		
+		HSVColor color = new HSVColor(shades);
+		color.findGoodCombination();
+		
+		// for each color
+		for (int c = 2; c < colors + 2; c++) {
+			color.resetShade();
+			// for each shade
+			for (int i = 0; i < shades; i++) {
+				colorScheme[i + shades * c] = HSVtoRGB(color.getHue(), color.getShadeSaturation(), color.getShadeValue());
+				color.shiftShade();
+			}
+			color.shiftHue();
+			color.shiftSaturationAndValue();
+		}
+		return colorScheme;
+	}
+	
+	private static void initializeColorScheme(int[] colorScheme, int transparent, int black, int shades, int colors){
 		double darken = (0.3 + 0.2 * random.nextDouble()) / (shades - 1);
 		for (int i = 0; i < shades; i++) {
 			colorScheme[i] = transparent;
 			colorScheme[i + shades] = black;
 			// generates shades of gray starting from white
-			HSVtoRGB(0, 0, 1.0 - darken * i, colorScheme, i + shades * (colors + 2));
+			colorScheme[i + shades * (colors + 2)] = HSVtoRGB(0, 0, 1.0 - darken * i);
 		}
-		double hshift=0;
-		double svshift=0; 
-		double s=0; 
-		double h=0;
-		boolean goodCombinationFound = false;
-		while (goodCombinationFound == false) { // find good combination
-
-			if (random.nextDouble() > 0.5) {
-				hshift = random.nextDouble(); // random colours
-			} else {
-				hshift = 0.1 * random.nextDouble(); // tight colour key
-			}
-			svshift = 0.5 * random.nextDouble();
-			s = 0.5 + 0.5 * random.nextDouble();
-			h = random.nextDouble();
-			// we want either big hshift or big svshift
-			if ((hshift < 0.2 || hshift > 0.8) && svshift < 0.2)
-				continue;
-
-			// if we have low s, we want svshift to be high enough to
-			// shift it to 1
-			if (s + svshift < 1)
-				continue;
-
-			// reject some pervasive greens
-			if (h >= 0.1666667 && h <= 0.4 && (hshift < 0.1 || hshift > 0.9)
-					&& random.nextDouble() > 0.5)
-				continue;
-
-			goodCombinationFound = true;
-		}
-		
-		svshift /= (shades - 1); // normalise over shades
-		double shadeshift = (0.4 + 0.3 * random.nextDouble()) / (shades - 1);
-		double v = 1;
-		for (int c = 2; c < colors + 2; c++) {
-			double ssh = s, vsh = v;
-			for (int i = 0; i < shades; i++) {
-				HSVtoRGB(h, ssh, vsh, colorScheme, i + shades * c);
-				if (ssh < 1) {
-					ssh += shadeshift;
-					if (ssh > 1) {
-						vsh -= ssh - 1;
-						ssh = 1;
-					}
-				} else {
-					vsh -= svshift;
-				}
-			}
-			h += hshift;
-			if (h > 1)
-				h -= 1;
-			if (s < 1) {
-				s += svshift;
-				if (s > 1) {
-					v -= s - 1;
-					s = 1;
-				}
-			} else {
-				v -= svshift;
-			}
-		}
-		return colorScheme;
 	}
 
+	private static class HSVColor {
+		double hue_shift = 0;
+		double saturation_value_shift = 0;
+		double shade_shift = 0;
+		double saturation = 0; 
+		double hue = 0;
+		double value = 1;
+		double shade_saturation = 0;
+		double shade_value = 0;
+		int shadeCount;
+		
+		public HSVColor(int shadeCount_){
+			shadeCount = shadeCount_;
+		}
+		
+		/**
+		 * Finds a good combination of parameters to generate a Color scheme.
+		 * @param shadesCount
+		 */
+		private void findGoodCombination(){
+			boolean goodConfigurationFound = false;
+			while (goodConfigurationFound == false) { // find good combination
+				// half of the time generate random schemes
+				// the other half generate closely related schemes
+				if (random.nextDouble() > 0.5) {
+					hue_shift = random.nextDouble(); // random colours
+				} else {
+					hue_shift = 0.2 * random.nextDouble(); // tight colour key
+				}
+				saturation_value_shift = 0.5 * random.nextDouble();
+				saturation = 0.5 + 0.5 * random.nextDouble();
+				hue = random.nextDouble();
+				value = 0.8 + 0.2 * random.nextDouble();
+
+				// If the hue_shift is extreme and the saturation_value_shift is small
+				// the combination is bad
+				if ((hue_shift < 0.2 || hue_shift > 0.8) && saturation_value_shift < 0.2){
+					continue;
+				}
+
+				// if we have low saturation, we want saturation_value_shift
+				// to be high enough to shift it to 1
+				if (saturation + saturation_value_shift < 1){
+					continue;
+				}
+
+				goodConfigurationFound = true;
+			}
+			saturation_value_shift /= shadeCount; // normalise over shades
+			shade_shift = (0.4 + 0.3 * random.nextDouble()) / shadeCount;
+		}
+		
+		/**
+		 * Resets the value of the shade to the base color.
+		 */
+		public void resetShade(){
+			shade_saturation = saturation;
+			shade_value = value;
+		}
+		
+		/**
+		 * Shifts the saturation and value of the color.
+		 */
+		private void shiftSaturationAndValue(){
+			if (saturation < 1) {
+				saturation += saturation_value_shift;
+				if (saturation > 1) {
+					value -= saturation - 1;
+					saturation = 1;
+				}
+			} else {
+				value -= saturation_value_shift;
+				// Notice that if the new value does not leave room for the new shades, they will underflow
+				// That is why we recalculate the value on this case
+				if(value < shade_shift*shadeCount){
+					value = 0.8 + 0.2 * random.nextDouble();
+					saturation = 0.5 + 0.5 * random.nextDouble();
+				}
+			}
+		}
+		
+		/**
+		 * Shifts the hue of the color.
+		 */
+		private void shiftHue(){
+			hue += hue_shift;
+			if (hue > 1){
+				hue -= 1;
+			}
+		}
+	
+		/**
+		 * Shifts the shade of the color.
+		 */
+		private void shiftShade(){
+			if (shade_saturation < 1) {
+				shade_saturation += shade_shift;
+				if (shade_saturation > 1) {
+					shade_value -= shade_saturation - 1;
+					shade_saturation = 1;
+				}
+			} else {
+				shade_value -= shade_shift;
+				// If the value goes below 0, recalculate at random. This shouldn't happen given that shiftSaturationAndValue is correct
+				// If it happens, the next shade will be lighter in color. And that is a problem if you assume that color are ordered
+				// by brightness.
+				if(shade_value < 0){
+					shade_value = 0.8 + 0.2 * random.nextDouble();
+					shade_saturation = 0.5 + 0.5 * random.nextDouble();
+				}
+			}
+		}
+		
+		public double getShadeSaturation() {
+			return shade_saturation;
+		}
+		
+		public double getHue() {
+			return hue;
+		}
+		
+		public double getShadeValue() {
+			return shade_value;
+		}
+	}
 }
--- src/examples/tools/spritegen/ColorSchemeVisualizer.java
+++ src/examples/tools/spritegen/ColorSchemeVisualizer.java
@@ -1,18 +1,20 @@
 package examples.tools.spritegen;
 
-import java.io.File;
-import java.util.Calendar;
 import java.util.Random;
 
 import jgame.JGColor;
-import jgame.JGFont;
-import jgame.JGObject;
 import jgame.JGPoint;
 import jgame.platform.JGEngine;
 
 /** Minimal shooter for jgame skeletons. */
 public class ColorSchemeVisualizer extends JGEngine {
 	static Random random = new Random();
+	int i1 = 123;
+	int i2 = 234;
+	int i3 = 345;
+	int i4 = 456;
+	int i5 = 567;
+	int i6 = 678;
 	private static final long serialVersionUID = 2698073082669353351L;
 	
 	public static void fixRandom(int seed) {
@@ -50,23 +52,33 @@ public class ColorSchemeVisualizer extends JGEngine {
 	}
 
 	public void paintFrame() {
-		paintScheme(123,60*0);
-		paintScheme(234,60*1);
-		paintScheme(345,60*2);
-		paintScheme(456,60*3);
-		paintScheme(567,60*4);
-		paintScheme(678,60*5);
+		if(getMouseButton(1)){
+			i1 = random.nextInt();
+			i2 = random.nextInt();
+			i3 = random.nextInt();
+			i4 = random.nextInt();
+			i5 = random.nextInt();
+			i6 = random.nextInt();
+		}
+		paintScheme(i1,60*0);
+		paintScheme(i2,60*1);
+		paintScheme(i3,60*2);
+		paintScheme(i4,60*3);
+		paintScheme(i5,60*4);
+		paintScheme(i6,60*5);
 	}
 	
 	public void paintScheme(int seed, int offset){
 		ColorSchemeGenerator.fixRandom(seed);
+		System.out.println("generate");
 		int[] c = ColorSchemeGenerator.genSpriteColorScheme(
-				SpriteGenerator.transcolor, 0, 3, 3);
+				SpriteGenerator.transcolor, 0, 3, 4);
+		//System.out.println(seed);
 		for(int i = 0; i < c.length; i++){
 			int b = c[i]&0xFF;
 			int g = c[i]>>8&0xFF;
 			int r = c[i]>>16&0xFF;
-			System.out.println(r+" "+g+" "+b);
+			//System.out.println(r+" "+g+" "+b);
 			JGColor boxColor = new JGColor(r, 
 					g,
 					b);
@@ -77,8 +89,8 @@ public class ColorSchemeVisualizer extends JGEngine {
 					50, 
 					true, 
 					false);
+			// Move the sprites to their position on the screen.
+			moveObjects(null, 0);
 		}
-		// Move the sprites to their position on the screen.
-		moveObjects(null, 0);
 	}
 }
