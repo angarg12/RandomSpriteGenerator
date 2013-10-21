dbc760b865b104b6697b5cea6ecde3cd64b8c980
Andrés <angarg12@gmail.com>
2013-10-21 16:26:18 +0200

    ColorSchemeGenerator Unit test fixed

--- src/examples/tools/spritegen/test/ColorSchemeGeneratorTest.java
+++ src/examples/tools/spritegen/test/ColorSchemeGeneratorTest.java
@@ -19,18 +19,22 @@ public class ColorSchemeGeneratorTest {
 		ColorSchemeGenerator.fixRandom(1000);
 		int[] colorScheme = ColorSchemeGenerator.genSpriteColorScheme(transparentColor,blackColor,colorCount,shadesCount);
 		int[] testScheme = genSpriteColorSchemeTable(transparentColor,blackColor,colorCount,shadesCount);
-		assertArrayEquals("Color array does not match the expected.", colorScheme, testScheme);
+		for(int i = 0; i < colorScheme.length; i++){
+			System.out.print(colorScheme[i]+",");
+		}
+		System.out.println();
+		assertArrayEquals("Color array does not match the expected.", testScheme, colorScheme);
 	}
 	
 	private int[] genSpriteColorSchemeTable(int transparentColor, int blackColor, int colorCount, int shadesCount){
 		if(transparentColor == 999 && blackColor == 0 && colorCount == 3 && shadesCount == 3){
-			return new int[] {999,999,999,0,0,0,12713793,10941952,10810368,8585023,5698560,5632256,4390716,520448,519936,16777215,13092807,9342606};
+			return new int[] {999,999,999,0,0,0,13445598,13240542,11534530,14603525,12498432,10064384,47546,37781,28271,16777215,13092807,9342606};
 		}
 		if(transparentColor == 0 && blackColor == 999 && colorCount == 2 && shadesCount == 4){
-			return new int[] {0,0,0,0,999,999,999,999,12713793,11665168,9821696,9755648,8650559,6553358,5102592,5102080,16777215,14277081,11842740,9342606};
+			return new int[] {0,0,0,0,999,999,999,999,13445598,13308126,12648661,11010233,14603534,13814016,11972096,10130176,16777215,14277081,11842740,9342606};
 		}
 		if(transparentColor == 200 && blackColor == 200 && colorCount == 4 && shadesCount == 2){
-			return new int[] {200,200,200,200,12713793,7646208,8519484,3909376,4063031,369152,3342189,40237,16777215,9342606};
+			return new int[] {200,200,200,200,13445598,12648661,13616640,9932800,36753,22617,15401949,11993259,16777215,9342606};
 		}
 		return null;
 	}
